3413c37777ddd80827933d7054d1cdefed3ac2ca
I am_Jiwon2 <126080767+YOONapply@users.noreply.github.com>
2024-10-08 14:24:48 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,17 +1,35 @@
-from random import shuffle
+# # from fastapi import FastAPI, Form
+# # from fastapi.responses import HTMLResponse
+# # from fastapi.routing import APIRouter
 
+# # app = FastAPI()
 
-def f():    
-    testList = []
-    for i in range(1, 101):
-        testList.append(i)
+# # @app.get("/", response_class=HTMLResponse)
+# # async def read_form():
+# #     return """
+# #     <form action="/submit" method="post">
+# #         <input type="text" name="value2" value="asd" placeholder="Enter some text" style="display: none">
+# #         <input type="submit" value="Submit">
+# #     </form>
+# #     """
 
-    shuffle(testList)
-    return testList
+# # @app.post("/submit")
+# # async def submit_form(value2: str = Form(...)):
+# #     return {"submitted_value": value2}
 
-l = f()
-print(l)
+# # if __name__ == "__main__":
+# #     import uvicorn
+# #     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-li = []
-li = f()
-print(li)
+# str_num = "123"
+# str_num = int(str_num)
+# str_num += 1
+# str(str_num)
+# str_num = str(str_num)
+# print(str_num)
+# print(type(str_num))
+
+total_try_count = 1
+total_wrong_count = 1
+user_now_rate = ( total_try_count - float(total_wrong_count) ) / total_try_count * 100.0
+print(user_now_rate)
